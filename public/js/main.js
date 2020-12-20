b70c4f6d864870767e890e4e05410f26bcb0c224
Jake Pelrah <jpelrah@wpi.edu>
2020-12-20 15:46:51 -0500

    Created Cursor

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,18 @@
-import NoiseGenerator from '../js/NoiseGenerator.js'
-import Canvas from '../js/Canvas.js'
+import getRandomInt from "../js/Random.js";
+import Cursor from "../js/Cursor.js"
 
-let img = new Image()
-img.src = 'images/svg/Pelrah_Jake.svg'
-let me = new Image()
-me.src = 'images/jpeg/me.jpeg'
 
-let width = img.width/2
-let height = img.height/2
+const mainCanvas = document.querySelector('#mainCanvas')
+const ctx = mainCanvas.getContext('2d')
+const terminalGreen = {r: 51, g: 255, b: 0, a: 255}
 
 
 
-let homeCanvas = new Canvas('#homeCanvas', width, height)
-let hiddenCanvas = new Canvas('#hiddenCanvas', width, height)
-let x = 600
 
-setInterval(()=>{
-    hiddenCanvas.ctx.drawImage(me, 0, 0, width, height)
-    let noise = new NoiseGenerator(hiddenCanvas.ctx.getImageData(0,0,width,height))
-    homeCanvas.ctx.putImageData(noise.randomAlpha(), 0,0)
-    homeCanvas.ctx.drawImage(img, 0, 0, width, height)
+//Flashing Cursor
+let cursor = new Cursor(ctx, 10, 20, terminalGreen)
+cursor.animate(0, 0)
 
-}, 10)
 
 
 
