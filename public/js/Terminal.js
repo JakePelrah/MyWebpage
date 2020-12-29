1e3071b370c131233b08f0da00c64c5840b87320
silver <jpelrah@wpi.edu>
2020-12-28 22:15:10 -0500

    Cursor works

--- public/js/Terminal.js
+++ public/js/Terminal.js
@@ -14,14 +14,14 @@ class Terminal extends Canvas {
         this.promptLen = this.getStringWidth(this.promptStr)
         this.kBuffer = new KeyBuffer(regex)
 
-        //String positions
-        this.textPos = new Position(this.getStringWidth(this.promptStr) + this.fontSize / 2, this.fontSize)
+        //Initial Positions
         this.promptPos = new Position(0, this.fontSize)
-        this.cursorPos = new Position(this.getStringWidth(this.promptStr) + this.fontSize / 2, (this.fontSize / 2) / 2)
+        this.textPos = new Position(this.getStringWidth(this.promptStr) + this.fontSize / 2, this.fontSize)
+        this.cursorPos = new Position(this.getStringWidth(this.promptStr) + this.fontSize ,  Math.ceil(this.fontSize/4))
 
         //Draw calls
         this.drawPrompt(this.promptPos.x, this.promptPos.y)
-        // this.drawCursor(this.cursorPos.x, this.cursorPos.y)
+        this.drawCursor(this.cursorPos.x, this.cursorPos.y)
 
         //Set listener for key input
         window.onkeydown = (ev) => {
@@ -30,13 +30,12 @@ class Terminal extends Canvas {
     }
 
 
-    drawPrompt(x, y) {
-        this.ctx.fillText(this.promptStr, x, y)
-    }
-
-
     getStringWidth(string) {
         return this.ctx.measureText(string).width
+    }
+
+    drawPrompt(x, y) {
+        this.ctx.fillText(this.promptStr, x, y)
     }
 
     drawLastChar(x, y) {
@@ -49,33 +48,49 @@ class Terminal extends Canvas {
     }
 
 
+    drawCursor(x, y) {
+        this.ctx.fillRect(x, y, this.fontSize / 2, this.fontSize)
+    }
+
+    clearCursor(x, y) {
+        this.ctx.clearRect(x, y, this.fontSize / 2, this.fontSize)
+    }
+
     update(ev) {
         switch (ev.key) {
             case 'Enter':
+                //clear the old cursors rect
+                this.clearCursor(this.cursorPos.x, this.cursorPos.y)
                 //process command in buffer
-                //Clear buffer contents
+
+                //clear buffer contents
                 this.kBuffer.clear()
                 //Move prompt, cursor and text output to next line
                 this.promptPos.y += this.fontSize
                 this.cursorPos.y += this.fontSize
                 this.textPos.y += this.fontSize
                 //Move cursor and text output back to a position after the prompt
-                this.cursorPos.x = this.getStringWidth(this.promptStr) + this.fontSize / 2
+                this.cursorPos.x = this.getStringWidth(this.promptStr) + this.fontSize
                 this.textPos.x = this.getStringWidth(this.promptStr) + this.fontSize / 2
                 this.drawPrompt(this.promptPos.x, this.promptPos.y)
+                this.drawCursor(this.cursorPos.x, this.cursorPos.y)
                 break
             case 'Backspace':
                 //Pop buffer
                 this.kBuffer.pop()
                 let string = this.kBuffer.toString()
-                //Move cursor and text position one chatac
                 this.cursorPos.x = this.promptLen + this.getStringWidth(string) + this.fontSize
                 this.textPos.x = this.promptLen + this.getStringWidth(string) + this.fontSize / 2
                 this.clearLastChar(this.cursorPos.x, this.cursorPos.y)
+                this.drawCursor(this.cursorPos.x, this.cursorPos.y)
+                this.clearCursor(this.cursorPos.x + this.fontSize/2, this.cursorPos.y)
                 break
             default:
                 if (this.kBuffer.push(ev)) {
                     this.textPos.x += this.fontSize / 2
+                    this.cursorPos.x += this.fontSize / 2
+                    this.drawCursor(this.cursorPos.x, this.cursorPos.y)
+                    this.clearCursor(this.cursorPos.x - this.fontSize/2 , this.cursorPos.y)
                     this.drawLastChar(this.textPos.x, this.textPos.y)
                 }
                 break
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
 import Terminal from '../js/Terminal.js';
 import NamedColor from '../js/NamedColor.js'
 
+//Use fonts divisible by two to prevent artifacts
 const canvasId = '#mainCanvas'
-const width = 800
-const height = 800
+const width = 600
+const height = 600
 const regex = /^[\w\s\.~`!@#$%^&*\\|(){}\[\]+=,/?<>:;'"_-]$/
-const termFont = '30px UbuntuMono'
+const termFont = '26px UbuntuMono'
 const promptStr = 'guest@jakepelrah:~$'
 
 const term = new Terminal(canvasId, width, height,
